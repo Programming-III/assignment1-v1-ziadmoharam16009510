e12fe087a3609a1e6ef194221b8f6249d2ce3fbf
ziadmoharam16009510 <ziad.elsayed@student.giu-uni.de>
2025-11-12 15:35:07 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,173 +1,129 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include "animal.H"
-#include "Mammal.H"
-#include "Bird.H"
-#include "Reptile.H"
-#include "enclosure.H"
-#include "visitor.H"
-
-
 using namespace std;
 
-class animal{
+class Animal {
+private:
+    string name;
+    int age;
+    bool isHungry;
 
-     private :
-     string name;
-     int age;
-     bool isHungry;
+public:
+    Animal() {}
 
-    public :
-    animal(){};
-
-    animal(string n, int a, bool H){
+    Animal(string n, int a, bool H) {
         name = n;
         age = a;
         isHungry = H;
-    };
-    
-    void display(){
-        string a;
-        if (isHungry){
-            a="Hungry";
-        }
-        else {
-            a="Not Hungry";
-        }
-        cout <<name<<"(Age : "<<age<<","<<a<<")"<<endl;
-    };
-    void feed(){
+    }
+
+    virtual ~Animal() {}
+
+    virtual void display() {
+        string hungerStatus = isHungry ? "Hungry" : "Not Hungry";
+        cout << name << " (Age: " << age << ", " << hungerStatus << ")" << endl;
+    }
+
+    void feed() {
         isHungry = false;
-
-    };
-
+    }
 };
-class Mammal : public animal{
-private :
-    string furcolor;
 
-public :
-    Mammal() : animal() {}
-    
-    Mammal(string n, int a, bool H) : animal(n, a, H) {}
-    
+class Mammal : public Animal {
+private:
+    string furColor;
+
+public:
+    Mammal() : Animal() {}
+    Mammal(string n, int a, bool H) : Animal(n, a, H) {}
 };
-class Bird : public animal{
-private :
+
+class Bird : public Animal {
+private:
     float wingspan;
 
-public :
-    Bird() : animal() {}
-    
-    Bird(string n, int a, bool H) : animal(n, a, H) {}
-    
+public:
+    Bird() : Animal() {}
+    Bird(string n, int a, bool H) : Animal(n, a, H) {}
 };
 
-class Reptile : public animal{
-private :
+class Reptile : public Animal {
+private:
     bool isVenomous;
 
-public :
-    Reptile() : animal() {}
-    
-    Reptile(string n, int a, bool H) : animal(n, a, H) {}
-    
+public:
+    Reptile() : Animal(), isVenomous(false) {}
+    Reptile(string n, int a, bool H) : Animal(n, a, H), isVenomous(false) {}
+
     void setVenomous(bool v) { isVenomous = v; }
-    
 };
 
-class enclosure{
-private :
-    vector<animal*> animals;
+class Enclosure {
+private:
+    vector<Animal*> animals;
     int capacity;
     int currentCount;
 
-public :
-    enclosure() {}
-
-    enclosure(int c, int cc)
-        : capacity(c), currentCount(cc)
-    {
+public:
+    Enclosure() : capacity(0), currentCount(0) {}
+    Enclosure(int c, int cc) : capacity(c), currentCount(cc) {
         animals.reserve(capacity);
     }
 
-
-    bool addAnimal(animal* a){
+    bool addAnimal(Animal* a) {
         if (!a) return false;
-        if (currentCount >= capacity) return false; 
+        if (currentCount >= capacity) return false;
         animals.push_back(a);
         ++currentCount;
         return true;
     }
 
     void displayAnimals() const {
-        for (int i = 0; i < currentCount && i < static_cast<int>(animals.size()); ++i) {
+        for (int i = 0; i < currentCount && i < (int)animals.size(); ++i) {
             if (animals[i]) animals[i]->display();
         }
     }
 };
-class visitor{
- private :
-     string visitorName;
-     int ticketsBought;
-    
 
-    public :
-    visitor(){};
-    visitor(string v, int t){
-       visitorName= v;
-       ticketsBought = t;
+class Visitor {
+private:
+    string visitorName;
+    int ticketsBought;
 
-    };
-    
-    void displayInfo(){
-        cout<<"Name : "<< visitorName << endl;
-        cout<<"Tickets Bought : "<< ticketsBought << endl;
-    };
+public:
+    Visitor() {}
+    Visitor(string v, int t) {
+        visitorName = v;
+        ticketsBought = t;
+    }
+
+    void displayInfo() {
+        cout << "Name: " << visitorName << endl;
+        cout << "Tickets Bought: " << ticketsBought << endl;
+    }
 };
 
+int main() {
+    Enclosure zoo(5, 0);
 
-int main(){
-    
-    enclosure zoo(5, 0);
-    
-    
-    mammal* lion = new mammal("Lion", 5, true);
-    
-    
-    bird* parrot = new bird("parrot", 2, false);
-    
-
-    reptile* snake = new reptile("snake", 3, true);
+    Mammal* lion = new Mammal("Lion", 5, true);
+    Bird* parrot = new Bird("Parrot", 2, false);
+    Reptile* snake = new Reptile("Snake", 3, true);
     snake->setVenomous(true);
 
-
-   
     zoo.addAnimal(lion);
-  
-    
     zoo.addAnimal(parrot);
-    
-    
-   zoo.addAnimal(snake);
-  
-    
-    
-    
+    zoo.addAnimal(snake);
+
     zoo.displayAnimals();
-    
-   
-    visitor guest("Sarah Ali", 3);
-    
-    
-   
+
+    Visitor guest("Sarah Ali", 3);
     guest.displayInfo();
-    
-  
+
     delete lion;
     delete parrot;
     delete snake;
-    
+
     return 0;
 }
